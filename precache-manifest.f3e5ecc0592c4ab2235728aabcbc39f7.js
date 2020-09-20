4c4ef037930ca954b8484c5533c71511dd5213b8
Hector Silva <hectddos103@gmail.com>
2020-09-20 14:04:23 -0300

    Correçao de responsividade dos graficos na dashboard

---
+++ precache-manifest.f3e5ecc0592c4ab2235728aabcbc39f7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7107142cbb6c60d93d3a97842796897a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fda686dca5d88ac9761",
+    "url": "/static/css/2.32713d93.chunk.css"
+  },
+  {
+    "revision": "ecb52913a2109622b336",
+    "url": "/static/css/main.5ab1bb98.chunk.css"
+  },
+  {
+    "revision": "4fda686dca5d88ac9761",
+    "url": "/static/js/2.3c2ff381.chunk.js"
+  },
+  {
+    "revision": "9b469fd2e6a5cc54ff6bbc28b0e8f457",
+    "url": "/static/js/2.3c2ff381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb52913a2109622b336",
+    "url": "/static/js/main.0da6da32.chunk.js"
+  },
+  {
+    "revision": "ff500c923b97b666a4c7",
+    "url": "/static/js/runtime-main.3fa9f258.js"
+  },
+  {
+    "revision": "736cbfddbb40fc15b6477ad0566e2b6c",
+    "url": "/static/media/abstract.736cbfdd.png"
+  },
+  {
+    "revision": "c5acbfc1fa8d037f264d7a821a6fac74",
+    "url": "/static/media/abstract_covid_box.c5acbfc1.png"
+  },
+  {
+    "revision": "77609f9c12d89bc65cdaadd0068b7f7f",
+    "url": "/static/media/back_circle.77609f9c.png"
+  },
+  {
+    "revision": "0a8a436add6e33e8d29c903fd4f4cab2",
+    "url": "/static/media/background_box.0a8a436a.png"
+  },
+  {
+    "revision": "3e2078cef4525a0d32e2e8aed0b301ba",
+    "url": "/static/media/background_boxgreen.3e2078ce.png"
+  },
+  {
+    "revision": "8455dd51fd7c01f40bf3e4c27cabca62",
+    "url": "/static/media/background_ogin.8455dd51.png"
+  },
+  {
+    "revision": "7cb825493b50b42ce8cf4610efddfa76",
+    "url": "/static/media/card.7cb82549.png"
+  },
+  {
+    "revision": "9074da4314e13398fd6b446a94a24ebb",
+    "url": "/static/media/card_code.9074da43.png"
+  },
+  {
+    "revision": "eb8fafdfc5e34c29a9822f4ca27df5fe",
+    "url": "/static/media/card_everse.eb8fafdf.png"
+  },
+  {
+    "revision": "24341b6ae35aa42fea49055021713226",
+    "url": "/static/media/logo_covid.24341b6a.png"
+  },
+  {
+    "revision": "6818a0ac7d40fddc203ba88ff889e120",
+    "url": "/static/media/subset-Graphik-Light.6818a0ac.woff2"
+  },
+  {
+    "revision": "88901c36112629f7a8ace8fff1220574",
+    "url": "/static/media/subset-Graphik-Medium.88901c36.woff2"
+  },
+  {
+    "revision": "ecc03d4e1efb387b0814de0c86d597be",
+    "url": "/static/media/subset-Graphik-Regular.ecc03d4e.woff2"
+  },
+  {
+    "revision": "99f75aa53e6a76415790d30ecf952a11",
+    "url": "/static/media/user_icon.99f75aa5.png"
+  }
+]);
